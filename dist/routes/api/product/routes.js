d83f633961a391ba40776c9592bb629a8d7a9920
Xr_v <xoji3029@gmail.com>
2023-10-06 23:32:57 +0500

    some editions

--- dist/routes/api/product/routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const controller_1 = require("./controller");
-const router = (0, express_1.Router)();
-const controller = new controller_1.ProductController();
-router.post("/create", controller.create);
-exports.default = router;
