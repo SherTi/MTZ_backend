cc7e4e80209b5c479db203397191d3d39bf2d4e8
SherTi <tulkunovsher69@gmail.com>
2023-10-06 17:44:25 +0500

    some editions

--- src/db.ts
+++ src/db.ts
@@ -2,11 +2,11 @@ import { Sequelize } from "sequelize";
 
 export default new Sequelize({
   host: "127.0.0.1",
-  port: 3306,
+  port: 3306, 
   dialect: "mysql",
   database: "mtz_tractor",
   username: "root",
-  password: "xoji1248000",
+  password: "M*N1u(n*jEZp5!XW",
   logging: false,
 });
 // xoji1248000
