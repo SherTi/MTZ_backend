be570e1e75c273133e56cd870dc1920aed74d1c6
Xr_v <xoji3029@gmail.com>
2024-01-21 03:55:48 +0500

    some editions

--- src/db.ts
+++ src/db.ts
@@ -4,9 +4,9 @@ export default new Sequelize({
   host: "localhost",
   port: 3306,
   dialect: "mysql",
-  database: "mtz_tractor",
-  username: "root",
-  password: "M*N1u(n*jEZp5!XW",
+  database: "brtradeu_gen_base",
+  username: "brtradeu_gen_base",
+  password: "BzsMztHs5B",
   logging: false,
 });
 
